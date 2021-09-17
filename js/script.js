9195753d9bb47e448a57b0e19c192b4f9248893d
ANGAHEE <heevel2@naver.com>
2021-09-17 22:27:59 +0900

    210917 swiper

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
-// $(function(){
-//     $(".slide>li").eq(0).siblings().css({ left: "-1480px" });
-//     let imageI = 0;
-//     setInterval(() => {
-//       if (imageI < 2) {
-//         imageI++;
-//       } else {
-//         imageI = 0;
-//       }
-//       $(".slide>li").eq(imageI).siblings().animate({ left: "-1480px" }, 500);
-//       $(".slide>li").eq(imageI).animate({ left: "0" }, 500);
-//     }, 3000);
+// document.querySelector('.btn2').addEventListener('click', function(){
+//     document.querySelector('.container').style.transform ='translate(-1480px)';
 // });
-
-document.querySelector('.btn2').addEventListener('click', function(){
-    document.querySelector('.container').style.transform ='translate(-1480px)';
-});
-document.querySelector('.btn3').addEventListener('click', function(){
-    document.querySelector('.container').style.transform ='translate(-2960px)';
-});
-document.querySelector('.btn1').addEventListener('click', function(){
-    document.querySelector('.container').style.transform ='translate(0)';
-});
+// document.querySelector('.btn3').addEventListener('click', function(){
+//     document.querySelector('.container').style.transform ='translate(-2960px)';
+// });
+// document.querySelector('.btn1').addEventListener('click', function(){
+//     document.querySelector('.container').style.transform ='translate(0)';
+// });
