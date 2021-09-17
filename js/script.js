3972d9a8870fe9ab094e242a3d9b7af6ac217445
ANGAHEE <heevel2@naver.com>
2021-09-18 03:02:42 +0900

    210918 마무리

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// document.querySelector('.btn2').addEventListener('click', function(){
-//     document.querySelector('.container').style.transform ='translate(-1480px)';
-// });
-// document.querySelector('.btn3').addEventListener('click', function(){
-//     document.querySelector('.container').style.transform ='translate(-2960px)';
-// });
-// document.querySelector('.btn1').addEventListener('click', function(){
-//     document.querySelector('.container').style.transform ='translate(0)';
-// });
